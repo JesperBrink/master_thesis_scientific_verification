33a49a43d7a65e0ecaf9872352e2a8110786c314
Mikkel Bak <mikkelbb@gmail.com>
2021-02-22 20:51:05 +0100

    add arguments to preprocess to make it easier to run on own computer

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,4 +1,6 @@
+import argparse
 import jsonlines
+import enum
 from removeparentheses import remove_parentheses
 from removesections import remove_sections
 from functools import reduce
@@ -15,11 +17,34 @@ def preprocess(data_set_path, out_put_path, *options):
                 preprocessed_doc = reduce(execute, options, data_point)
                 output.write(preprocessed_doc)
 
+class Preprocessor(enum.Enum):
+    Parantheses = "paren"
+    Section = "section"
+
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create bar plots of evidence count")
+    parser.add_argument(
+        "corpus_path", metavar="path", type=str, help="the path to the corpus"
+    )
+    parser.add_argument(
+        "output_path", metavar="path", type=str, help="the path to the corpus"
+    )
+    parser.add_argument(
+        "preprocessors", metavar="path", type=Preprocessor, help="the path to the corpus", nargs="+"
+    )
+
+    args = parser.parse_args()
+    preprocessors = []
+    for preprocessor in args.preprocessors:
+        if preprocessor == Preprocessor.Parantheses:
+            preprocessors.append(lambda d: remove_parentheses(d, 2))
+        elif preprocessor == Preprocessor.Section:
+            preprocessors.append(lambda d: remove_sections(d))
+
+
     preprocess(
-        "../datasets/scifact/corpus.jsonl",
-        "temp.jsonl",
-        lambda d: remove_parentheses(d, 2),
-        lambda d: remove_sections(d),
+        args.corpus_path,
+        args.output_path,
+        *preprocessors
     )
