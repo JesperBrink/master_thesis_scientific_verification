e103d294678514914a12761c878446d386e9e01a
Mikkel Bak <mikkelbb@gmail.com>
2021-02-28 11:19:28 +0100

    update to lower

--- preprocessing/removestopwords.py
+++ preprocessing/removestopwords.py
@@ -7,6 +7,6 @@ STOPWORDS = set(stopwords.words("english"))
 def remove_stopwords(doc):
     for sentence_idx, sentence in enumerate(doc["abstract"]):
         doc["abstract"][sentence_idx] = " ".join(
-            [w for w in sentence.split() if w not in STOPWORDS]
+            [w for w in sentence.split() if w.lower() not in STOPWORDS]
         )
     return doc
