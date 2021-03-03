71168b2a65e01654011817f85cb761bc7ee6b2a8
Manuel R. Ciosici <manuelrciosici@gmail.com>
2021-03-03 11:06:45 -0500

    Speed up corpus embedding

--- preprocessing/embed.py
+++ preprocessing/embed.py
@@ -1,8 +1,12 @@
 from sentence_transformers import SentenceTransformer
 
+MODEL = None
+
 
 def s_bert_embed(doc):
-    model = SentenceTransformer("stsb-distilbert-base")
-    new_abstract_list = model.encode(doc["abstract"])
+    global MODEL
+    if MODEL is None:
+        MODEL = SentenceTransformer("stsb-distilbert-base")
+    new_abstract_list = MODEL.encode(doc["abstract"])
     doc["abstract"] = new_abstract_list.tolist()
     return doc
