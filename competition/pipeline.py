16aea531d6ddac1232185736c0319046fe34556f
Jesper Brink <jesper@brinkandersen.dk>
2021-03-04 21:11:57 +0100

    Pipeline can now produces prediction.jsonl file, which can be used for the scifact eval script

--- competition/pipeline.py
+++ competition/pipeline.py
@@ -13,13 +13,7 @@ def process_line(claim, line):
     return res
 
 
-def write_claim_id_to_rationale_dict_to_file(claim_id_to_rationale_dict):
-    with jsonlines.open("sentence_selection_result.jsonl") as output:
-        for claim_id, evidence in claim_id_to_rationale_dict.items():
-            output.write({"claim_id": claim_id, "evidence": evidence})
-
-
-def find_relevant_sentences(claim, corpus, model):
+def sentence_selection(claim, corpus, model):
     """ Returns a dict that maps abstract ids to relevant sentences ids in that abstract 
     i.e. {abstract_42: [sent_3, sent_7], abstract_127: [sent_4]}
     We have at most 9 sentences per abstract
@@ -35,72 +29,74 @@ def find_relevant_sentences(claim, corpus, model):
     # end todo
     
     predicted = model(sentence_embeddings)
-    res_mask = tf.squeeze(tf.math.greater(predicted, tf.constant(0.8)))
+    res_mask = tf.squeeze(tf.math.greater(predicted, tf.constant(0.999999)))
     res = tf.where(res_mask)
     
     relevant_sentences_dict = dict()
     for pred_id in res:
         abstract_id, sentence_id = corp_id[pred_id[0]] 
         sentence_list = relevant_sentences_dict.get(abstract_id, [])
-        sentence_list.append(sentence_id)
-        if len(sentence_list) > 9:
+        if len(sentence_list) == 9:
             print("Sentence list is larger than 9. FIX THIS")
+            continue
+        sentence_list.append({"id": sentence_id, "embedding": sentence_embeddings[pred_id].tolist()})
         relevant_sentences_dict[abstract_id] = sentence_list
-    
-    print(claim["id"])
-    print(relevant_sentences_dict)
-
+   
     return relevant_sentences_dict
-    #temp = [predicted[i[0]] for i in res]
-    #if len(temp):
-    #    print(sum(temp) / len(temp))
-    #else:
-    #    print("no corp found")
-    #gold = claim['evidence']
-    #found = [corp_id[i[0]] for i in res]
-    #print(claim['id'], len(found), [int(key) in found for key in gold.keys()])
-    #return None
 
 
-def sentence_selection(claims_path, corpus_path):
-    """ 
-    input: all sentences in the corpus
-    output: sentences that score above threshold (0.5) in being relevant
-    """
-    model = load()
-    claim_id_to_rationale_dict = dict() # claim_id: {abstract_42: {sent_3, sent_7}, abstract_127: {sent_4}}
-    corp = None
-    with jsonlines.open(corpus_path) as corpus:
-        corp = np.array(list(corpus.iter()))
-    print("loaded model and started")
-
-    with jsonlines.open(claims_path) as claims:
-        for claim in tqdm(claims):
-            relevant_sentences_dict = find_relevant_sentences(claim, corp, model)
-            claim_id_to_rationale_dict[claim["id"]] = relevant_sentences_dict
-
-    return claim_id_to_rationale_dict
+def same_prediction_as_avg(avg, pred):
+    if avg < 0.5 and pred < 0.5:
+        return True
+    elif avg >= 0.5 and pred >= 0.5:
+        return True
+    return False
 
 
-def stance_prediction():
+def stance_prediction(claim, evidence, model):
     """
     input: Claims + Rationales (from sentence selection)
     output: Whether abstracts/sentences support or refute claims
     """
-    # gennemsnit eller alt ind på en gang?
-    pass
+    claim_id = claim["id"]
+    
+    if not evidence:
+        return {"id": claim_id, "evidence": {}}
+    
+    resulting_evidence_dict = dict()
+    for abstract in evidence.keys():
+        stance_predictions = []
+        pred_sum = 0
+        predicted_sentences = evidence[abstract]
+        for sentence_dict in predicted_sentences:
+            embedding = np.array(sentence_dict["embedding"])
+            embedding = np.expand_dims(embedding, 0)
+            pred = model.predict(embedding)[0][0]
+            stance_predictions.append((sentence_dict["id"], pred))
+            pred_sum += pred
+        avg = pred_sum / len(stance_predictions)
+        rationale_sentences = [sent_id for sent_id, pred in stance_predictions if same_prediction_as_avg(avg, pred)]
+        label = "SUPPORT" if avg >= 0.5 else "CONTRADICT"
+        resulting_evidence_dict[str(abstract)] = {"sentences": rationale_sentences, "label": label}
+
+    return {"id": claim_id, "evidence": resulting_evidence_dict}   
 
 
-def parse_to_output():
-    # TODO: tjek hvordan vi skal skrive at vi ikke har et bud
-    pass
+def run_pipeline(corpus_path, claims_path):
+    abstract_retriever_model = load()
+    stance_prediction_model = load() # FIX THIS! De loader den samme model lige PT
+    with jsonlines.open(corpus_path) as corpus_reader:
+        corpus = np.array(list(corpus_reader.iter()))
 
-
+    with jsonlines.open("predictions.jsonl", "w") as output:
+        with jsonlines.open(claims_path) as claims:
+            for claim in tqdm(claims):
+                relevant_sentences_dict = sentence_selection(claim, corpus, abstract_retriever_model)
+                prediction = stance_prediction(claim, relevant_sentences_dict, stance_prediction_model) 
+                output.write(prediction)
+    
 if __name__ == '__main__':
     corpus_path = 'sbert-embedded-corpus.jsonl'
     claims_path= 'sbert-embedded-dev-claims.jsonl'
     
-    claim_id_to_rationale_dict = sentence_selection(claims_path, corpus_path)
-    write_claim_id_to_rationale_dict_to_file(claim_id_to_rationale_dict)
-    # print(claim_id_to_rationale_dict)
-    
+    run_pipeline(corpus_path, claims_path)
--- models/stance_prediction/model.py
+++ models/stance_prediction/model.py
@@ -1,8 +1,21 @@
+import numpy as np
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras import Input
+from tensorflow.keras.models import Sequential
 import tensorflow as tf
+from pathlib import Path
+from datasets.sentenceLevelAbstractRetrievalDataset.loadDataset import load_dataset, load_validation_dataset
+from models.utils import get_highest_count, setup_tensorboard
 
-class TwoLayerAbstractRetriever(tf.keras.Model):
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+
+_model_dir = Path(os.path.realpath(__file__)).resolve().parents[1] / "trained_models/stance_predictor"
+
+
+class TwoLayerStancePredictor(tf.keras.Model):
     def __init__(self, units):
-        super(TwoLayerAbstractRetriever, self).__init__()
+        super(TwoLayerStancePredictor, self).__init__()
         self.layer_1 = Dense(units, activation="relu")
         self.layer_2 = Dense(units, activation="relu")
         self.classifier = Dense(1, activation="sigmoid")
@@ -11,3 +24,47 @@ class TwoLayerAbstractRetriever(tf.keras.Model):
         x = self.layer_1(inputs)
         x = self.layer_2(x)
         return self.classifier(x)
+
+
+def load():
+    count = get_highest_count(_model_dir)
+    path = str(_model_dir / 'TwoLayerStancePredictor_{}'.format(count))
+    model = tf.keras.models.load_model(path)
+    return model
+
+
+def save(model):
+    count = get_highest_count(_model_dir) + 1
+    path = str(_model_dir / 'TwoLayerStancePredictor_{}'.format(count))
+    model.save(path)
+    print('model saved to {}'.format(path))
+
+
+def main():
+    BATCH_SIZE = 32
+    tensorboard_callback = setup_tensorboard()
+    loss = tf.keras.losses.BinaryCrossentropy()
+    m = TwoLayerStancePredictor(1024)
+    m.build((BATCH_SIZE, 1536))
+    m.compile(optimizer="adam", loss=loss, metrics=["accuracy"])
+    m.summary()
+    #dataset = load_dataset().batch(BATCH_SIZE, drop_remainder=True)
+    #validation_dataset = load_validation_dataset().batch(BATCH_SIZE, drop_remainder=True)
+    # early_stopping =tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+
+    #m.fit(
+    #    dataset, 
+    #    validation_data=validation_dataset,
+    #    epochs=17,
+    #    callbacks=[tensorboard_callback],
+    #    class_weight={0:1,1:50}
+    #)
+    
+    save(m)
+
+    #loaded_model = load()
+    #loaded_model.summary()
+
+
+if __name__ == "__main__":
+    main()
