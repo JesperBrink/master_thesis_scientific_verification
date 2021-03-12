42a467e4f71d9c4d234eb8d816892c51b1093e26
Mikkel S <mschou1306@gmail.com>
2021-03-12 15:43:06 +0100

    Add tooltips, use run id instead of name

--- hyperparameter_tuning/dashboard/pages/index.js
+++ hyperparameter_tuning/dashboard/pages/index.js
@@ -14,26 +14,20 @@ export default function Home() {
         const fileReader = new FileReader();
         fileReader.readAsText(event.target.files[0])
         fileReader.onload = ev => {
-            parseResultsFile(ev.target.result, event.target.files[0].name)
+            parseResultsFile(ev.target.result)
         };
     }
 
-    const parseResultsFile = (data, fullFileName) => {
+    // lav tooltip på hele row med ordentligt formaterede hyper parameters
+
+    const parseResultsFile = (data) => {
         let tempRuns = [];
-        const fileName = fullFileName.split(/[.-]/).slice(1,3).join("_"); // TODO: Make more general
 
         // Potential speed up here
-        data.split("\n").forEach(el => {
+        data.split("\n").forEach((el, i) => {
             if (el) {
-                let name = fileName;
                 let run = JSON.parse(el);
-
-                if (run.params) {
-                    for (const [key, value] of Object.entries(run.params)) {
-                        name = name.concat(`_${key}_${value}`);
-                    }
-                }
-                run["name"] = name;
+                run["id"] = i;
                 
                 tempRuns.push(run);
             }
@@ -48,35 +42,60 @@ export default function Home() {
 
     // update rows and columns when runs updates
     useEffect(() => {
-        setColumns(getColumns());
-        setRows(getRows());
+        updateColumns();
+        updateRows();
     }, [runs])
 
-    const getColumns = () => {
+    const formatParamsTooltip = (id) => {
+        let tooltip = []
+
+        if (runs[id].params) {
+            for (const [key, value] of Object.entries(runs[id].params)) {
+                if (key === "id") {
+                    continue;
+                }
+                
+                tooltip.push(<h2 key={key}>{key + ": " + value + "\n"}</h2>)
+            }
+        }
+
+        return tooltip;
+    }
+
+    const updateColumns = () => {
         let cols = [];
         if (runs.length > 0) {
+
             cols.push({
-                field: "name", 
+                field: "id", 
                 flex: 0.5,
                 renderCell: (params) =>  (
-                    <Tooltip title={params.row.name} >
-                        <span className="table-cell-trucate">{params.row.name}</span>
+                    <Tooltip title={formatParamsTooltip(params.row.id)} interactive>
+                        <span>{params.row.id}</span>
                     </Tooltip>
                 )
             });
             
             Object.keys(runs[0].results).forEach(key => {
-                cols.push({field: key, flex: 1, type: 'number'}); // add headerName if we want aliases, remove type if we want full values (also ruins result filtering)
+                cols.push({field: key, 
+                    flex: 1, 
+                    renderCell: (params) =>  (
+                        <Tooltip title={<h2>{params.row[key]}</h2>}>
+                            <span>{parseFloat(params.row[key]).toFixed(5)}</span>
+                        </Tooltip>
+                    )
+                }); // add headerName if we want aliases, remove type if we want full values (also ruins result filtering)
             })
         }
-        return cols;
+        setColumns(cols);
     }
         
-    const getRows = () => {
+    const updateRows = () => {
         let rows = [];
         if (runs.length > 0) {
+            // Create rows from runs
             for (let i = 0; i < runs.length; i++) {
-                let row = {id: i, name: runs[i]["name"]};
+                let row = {id: runs[i].id};
 
                 for (const [key, value] of Object.entries(runs[i].results)) {
                     row[key] = value;
@@ -85,13 +104,13 @@ export default function Home() {
                 rows.push(row);
             }
         }
-        return rows;
+        setRows(rows);
     }
 
     // TODO: Checlist:
-        // row with multi-dropdowns to filter data (skal egentlig være over table)
+        // multi-dropdowns to filter data by params
         // lav dem baseret på indlæst data? I.e. tag værdierne fra daten i stedet for hardcoded. Lad være med at vise dropdown hvis der ikke er nogle værdier (løser problemet med at stance-selection har færre hyperparameters)
-        // select rows (in table) to compare in another table (which is only shown if at least two datapoints are selected)
+        // MAYBE: select rows (in table) to compare in another table (which is only shown if at least two datapoints are selected)
 
     return (
         <div style={{ height: '100%' }}>
@@ -106,13 +125,13 @@ export default function Home() {
                     <UploadButton readResultsFile={readResultsFile}/>
                 </Grid>
                 <Grid item xs>
-                    {"Dropdowns"}
+                    {runs.length > 0 && <span>Dropdowns</span>}
                 </Grid>
                 <Grid item xs style={{width: '80%'}}>
-                    {<DataGrid 
+                    {runs.length > 0 &&<DataGrid 
                         rows={rows}
                         columns={columns} 
-                        pageSize={15} 
+                        pageSize={14} 
                         autoHeight={true}
                     />}
                 </Grid>
