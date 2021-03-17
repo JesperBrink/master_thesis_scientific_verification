87d9a3ca40fed49a252d0e6f5e43d32d2502d4b0
Mikkel Schou <33804514+mschou1306@users.noreply.github.com>
2021-03-17 10:10:47 +0100

    Feature/hyperparameter results dashboard (#21)
    * Add node server (+ Next.js + React) with initial dashboard setup
    
    * Add upload button. Parse jsonl input from upload and save to state
    
    * Generate names for each hyperparameter run
    
    * Fill table with rows and columns
    
    * Show name when hovering name cell
    
    * Add dark mode
    
    * Add tooltips, use run id instead of name
    
    * Update package.json
    
    * Move table and corresponding logic to separate component
    
    * Update function names
    
    * Remove set state from props anti-pattern in table component
    
    * Rewrite grid structure
    
    * Move upload button to bottom. Add dropdowns based on hyperparam keys and values
    
    * Format tooltips with div instead of h2. Autoformat all files
    
    * Add filtering of data, update tooltips to show proper params
    
    * Title of first row of filters is now not cut off
    
    * Reset filters when uploading new data
    
    * Update value check in Filters
    
    * Add node server (+ Next.js + React) with initial dashboard setup
    
    * Add upload button. Parse jsonl input from upload and save to state
    
    * Generate names for each hyperparameter run
    
    * Fill table with rows and columns
    
    * Show name when hovering name cell
    
    * Add dark mode
    
    * Add tooltips, use run id instead of name
    
    * Update package.json
    
    * Move table and corresponding logic to separate component
    
    * Update function names
    
    * Remove set state from props anti-pattern in table component
    
    * Rewrite grid structure
    
    * Move upload button to bottom. Add dropdowns based on hyperparam keys and values
    
    * Format tooltips with div instead of h2. Autoformat all files
    
    * Add filtering of data, update tooltips to show proper params
    
    * Title of first row of filters is now not cut off
    
    * Reset filters when uploading new data
    
    * Update value check in Filters
    
    * Fix error when params is missing
    
    * Update readme to include data specification
    
    * remove leftover merge string
    
    * Revert check for when filters show
    
    * Fix readme

---
+++ hyperparameter_tuning/dashboard/components/Filters.js
@@ -0,0 +1,31 @@
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+
+export default function Filters({ hyperparams, onChange, filters }) {
+    const getFilterDropdowns = () => {
+        let filterDropdowns = [];
+
+        for (const [key, values] of Object.entries(hyperparams)) {
+            filterDropdowns.push(
+                <Grid item xs={4} key={key}>
+                    <Autocomplete
+                        id={`filter_${key}`}
+                        options={Array.from(values)}
+                        onChange={(event, value, reason) => onChange(value, key)}
+                        multiple
+                        value={Array.from(values).filter(val => filters[key] && filters[key].includes(val))}
+                        getOptionLabel={(option) => option.toString()}
+                        renderInput={(params) => <TextField {...params} label={key} variant="outlined"/>}
+                    />
+                </Grid>
+            )
+        }
+
+        return filterDropdowns;
+    }
+
+    return (
+        getFilterDropdowns()
+    )
+}
