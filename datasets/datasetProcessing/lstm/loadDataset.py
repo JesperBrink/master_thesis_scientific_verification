0bdb38833a502f14afdb5eaae9514439834d69bc
Mikkel Bak <mikkelbb@gmail.com>
2021-03-26 09:37:56 +0100

    update tfrecord schema

--- datasets/datasetProcessing/lstm/loadDataset.py
+++ datasets/datasetProcessing/lstm/loadDataset.py
@@ -15,12 +15,12 @@ def read_relevance_tfrecord(serialized_example):
     feature_description = {
         "sequence": tf.io.VarLenFeature(tf.float32),
         "label": tf.io.VarLenFeature(tf.int64),
+        "shape": tf.io.FixedLenFeature([2], tf.int64)
     }
 
     example = tf.io.parse_single_example(serialized_example, feature_description)
-
-    X = example["sequence"]
-    Y = example["label"]
+    X = tf.reshape(tf.sparse.to_dense(example["sequence"]), example["shape"])
+    Y = tf.sparse.to_dense(example["label"])
 
     return X, Y
 
@@ -29,12 +29,13 @@ def read_lstm_tfrecord_dataset():
     filenames = [str(trainint_set_path / x) for x in os.listdir(trainint_set_path)]
     dataset = tf.data.TFRecordDataset(filenames)
 
-    return dataset.shuffle(30000).map(read_relevance_tfrecord)
+    return dataset.map(read_relevance_tfrecord)
 
 
 if __name__ == "__main__":
     count = 0
     for x in read_lstm_tfrecord_dataset():
-        assert x[0].shape[0] / x[1].shape[0] == 768 * 2
+        print(x[0].shape, x[1].shape)
+        assert x[0].shape[1] == 768 * 2
         count += 1
     print(count)
