44b880bb50e88efd4982e5050fab592b012fcee8
Mikkel Bak Bertelsen <mikkelbb@manjaro.local>
2021-03-27 10:08:33 +0100

    update model and loading

--- models/sentence_selection/lstmModel.py
+++ models/sentence_selection/lstmModel.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import tensorflow as tf
 from models.utils import get_highest_count, setup_tensorboard
-from datasets.datasetProcessing.lstm.loadDataset import read_lstm_tfrecord_dataset
+from datasets.datasetProcessing.lstm.loadDataset import load_lstm_validation_set, load_lstm_train_set
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 
@@ -13,28 +13,43 @@ _model_dir = (
 )
 
 
+if not tf.test.is_gpu_available:
+    print("tf not availabel")
+    exit()
+
 class TwoLayerAbstractRetriever(tf.keras.Model):
     def __init__(self, units):
         super(TwoLayerAbstractRetriever, self).__init__()
         self.layer_1 = tf.keras.layers.LSTM(
-            units, recurrent_activation="relu", recurrent_initializer="glorot_uniform"
+            units, return_sequences=True, recurrent_initializer="glorot_uniform"
         )
-        self.classifier = tf.keras.layers.Dense(1, activation="sigmoid")
+        # self.layer_2 = tf.keras.layers.LSTM(
+        #     units, recurrent_activation="relu", recurrent_initializer="glorot_uniform"
+        # )
+        self.classifier = tf.keras.layers.Dense(1, activation="softmax")
 
     def call(self, inputs):
         x = self.layer_1(inputs)
+        # x = self.layer_2(inputs)
         return self.classifier(x)
 
 
 def train(model, batch_size, epochs):
-    dataset = (
-        read_lstm_tfrecord_dataset()
+    data_set = (
+        load_lstm_train_set()
+        .shuffle(10000)
+        .batch(batch_size, drop_remainder=True)
+    )
+
+    validation_set = (
+        load_lstm_validation_set()
         .shuffle(10000)
         .batch(batch_size, drop_remainder=True)
     )
 
     model.fit(
-        dataset,
+        data_set,
+        validation_data=validation_set,
         epochs=epochs,
     )
 
@@ -68,7 +83,7 @@ if __name__ == "__main__":
 
     m = initialize_model(BATCH_SIZE, 512)
 
-    m = train(m, BATCH_SIZE, 1)
+    m = train(m, BATCH_SIZE, 10)
 
     save(m)
 
