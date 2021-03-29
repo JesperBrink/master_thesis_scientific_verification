1c3100250318afec0f928677a3d1c5d79870a80c
Mikkel S <mschou1306@gmail.com>
2021-03-29 17:45:26 +0200

    Move cosine similarity calculation to separate function

--- models/sentence_selection/cosine_similarity_model.py
+++ models/sentence_selection/cosine_similarity_model.py
@@ -2,11 +2,14 @@ import tensorflow as tf
 
 class CosineSimilaritySentenceSelector():
     def __call__(self, claim_sent_embeddings):
-        sentence_embeddings = claim_sent_embeddings[:,768:]
         claim_embedding = claim_sent_embeddings[0, :768]
+        sentence_embeddings = claim_sent_embeddings[:,768:]
         
-        norm_sentences = tf.math.l2_normalize(sentence_embeddings, 1)
-        norm_claim = tf.math.l2_normalize(claim_embedding, 0)
-        similarities = tf.reshape(tf.linalg.matvec(norm_sentences, norm_claim), (sentence_embeddings.shape[0], 1))
+        return tf.reshape(get_cosine_similarities(claim_embedding, sentence_embeddings), (sentence_embeddings.shape[0], 1))
 
-        return similarities
+def get_cosine_similarities(claim_embedding, sentence_embeddings):
+    norm_sentences = tf.math.l2_normalize(sentence_embeddings, 1)
+    norm_claim = tf.math.l2_normalize(claim_embedding, 0)
+    similarities = tf.linalg.matvec(norm_sentences, norm_claim)
+
+    return similarities
