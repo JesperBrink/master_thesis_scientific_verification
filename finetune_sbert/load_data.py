339d34ab26fd64bbf7314e4d23285a39cb94158b
Jesper Brink <jesper@brinkandersen.dk>
2021-04-01 13:13:19 +0200

    Added fever training data

--- finetune_sbert/load_data.py
+++ finetune_sbert/load_data.py
@@ -6,28 +6,25 @@ from sentence_transformers import InputExample
 from sentence_transformers import evaluation
 
 
-def load_training_data(claims_path, corpus_path):
+def load_training_data(scifact_claims_path, scifact_corpus_path, fever_claims_path):
     """ Both claim_path and corpus_path should be the text versions and not embedded versions """
     #return [InputExample(texts=['My first sentence', 'My second sentence'], label=0.8),
     #InputExample(texts=['Another pair', 'Unrelated sentence'], label=0.3)]
 
-    training_data = []
+    scifact_training_data = []
 
-    scifact_relevant_data = create_scifact_relevant(claims_path, corpus_path)
+    scifact_relevant_data = create_scifact_relevant(scifact_claims_path, scifact_corpus_path)
     scifact_relevant_data_train_format = convert_to_train_format(scifact_relevant_data)
-    training_data.extend(scifact_relevant_data_train_format)
+    scifact_training_data.extend(scifact_relevant_data_train_format)
 
-    scifact_not_relevant_data = create_scifact_not_relevant(claims_path, corpus_path, 5)
+    scifact_not_relevant_data = create_scifact_not_relevant(scifact_claims_path, scifact_corpus_path, 5)
     scifact_not_relevant_data_train_format = convert_to_train_format(scifact_not_relevant_data)
-    training_data.extend(scifact_not_relevant_data_train_format)
+    scifact_training_data.extend(scifact_not_relevant_data_train_format)
 
-    #fever_relevant_data = create_fever_relevant(claims_path, corpus_path)
-    #training_data.extend(fever_relevant_data)
+    fever_data = create_fever(fever_claims_path)
+    fever_data_train_format = convert_to_train_format(fever_data)
 
-    #fever_not_relevant_data = create_fever_not_relevant(claims_path, corpus_path, 5)
-    #training_data.extend(fever_not_relevant_data)
-
-    return training_data
+    return scifact_training_data, fever_data_train_format
     
 
 def load_evaluator(validation_claims_path, corpus_path):
@@ -149,3 +146,46 @@ def create_scifact_not_relevant(claim_path, corpus_path, k):
                     #not_relevant_data.append(InputExample(texts=[claim["claim"], sentence], label=0.0))
 
     return not_relevant_data
+
+
+def create_fever(claim_path):
+    fever_data = []
+
+    for claim in tqdm(jsonlines.open(claim_path)):
+        #claim_embedding = MODEL.encode(claim["claim"]).tolist()
+        # create a not relevant datapoint if not enough info
+        if claim["label"] == "NOT ENOUGH INFO":
+            allowed = [x for x in claim["sentences"] if x != ""]
+            negative_evidence = sample(allowed, min(len(allowed), 2))
+            for sentence in negative_evidence:
+                fever_data.append((claim["claim"], sentence, 0.0))
+            continue
+
+        # clean sentences by removing the part with the references.
+        cleaned_sentences = list(
+            map(lambda a: a.split(" . ")[0] + ".", claim["sentences"])
+        )
+        # set label to 1 if the claim is supported by abstract, else 0
+        # else create a relevant and not relevant datapoints from the sentence used to label
+        for evidence in claim["evidence_sets"]:
+            all_evidence = set()
+            for index in evidence:
+                all_evidence = all_evidence.union(set(evidence))
+
+            # write to the relevant dataset
+            usefull = [cleaned_sentences[index] for index in evidence]
+            for sentence in usefull:
+                fever_data.append((claim["claim"], sentence, 1.0))
+
+            # write to the not_relevant dataset
+            not_relevant_sents = set(range(len(cleaned_sentences))) - set(all_evidence)
+            allowed = [
+                cleaned_sentences[index]
+                for index in not_relevant_sents
+                if cleaned_sentences[index] != ""
+            ]
+            chosen = sample(allowed, min(2, len(allowed)))
+            for sentence in chosen:
+                fever_data.append((claim["claim"], sentence, 0.0))
+
+    return fever_data
