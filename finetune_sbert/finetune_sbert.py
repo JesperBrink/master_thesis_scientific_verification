c44c7e58a96b7777a7aeb3068e43d0967f232f08
Jesper Brink <jesper@brinkandersen.dk>
2021-04-02 11:22:59 +0200

    fixed error in previous commit

--- finetune_sbert/finetune_sbert.py
+++ finetune_sbert/finetune_sbert.py
@@ -2,20 +2,20 @@ from sentence_transformers import SentenceTransformer, losses
 from torch.utils.data import DataLoader
 from load_data import load_training_data, load_evaluator
 
-model_save_path = "finetuned-stsb-distilbert-base"
+model_save_path = "stsb-distilbert-base-finetuned-feverdev3-scifact20"
 scifact_claims_path = "../datasets/scifact/claims_sub_train.jsonl"
 scifact_val_claims_path = "../datasets/scifact/claims_validation.jsonl"
 scifact_corpus_path = "../datasets/scifact/corpus.jsonl"
-fever_claims_path = "../datasets/fever/fever_train.jsonl"
+#fever_claims_path = "../datasets/fever/fever_train.jsonl"
+fever_claims_path = "../datasets/fever/fever_dev.jsonl"
 
 
 model = SentenceTransformer("stsb-distilbert-base")
+train_loss = losses.CosineSimilarityLoss(model)
 
 scifact_training_data, fever_training_data = load_training_data(scifact_claims_path, scifact_corpus_path, fever_claims_path)
 scifact_train_dataloader = DataLoader(scifact_training_data, shuffle=True, batch_size=32)
 fever_train_dataloader = DataLoader(fever_training_data, shuffle=True, batch_size=32)
-
-train_loss = losses.CosineSimilarityLoss(model)
 evaluator = load_evaluator(scifact_val_claims_path, scifact_corpus_path)
 
 model.fit(
@@ -27,10 +27,11 @@ model.fit(
 )
 
 scifact_model = SentenceTransformer(model_save_path + "-fever")
+scifact_train_loss = losses.CosineSimilarityLoss(scifact_model)
 
 scifact_model.fit(
-    train_objectives=[(scifact_train_dataloader, train_loss)],
-    epochs=0,
+    train_objectives=[(scifact_train_dataloader, scifact_train_loss)],
+    epochs=20,
     evaluator=evaluator,
     evaluation_steps=500,
     output_path=model_save_path + "-fever-scifact",
