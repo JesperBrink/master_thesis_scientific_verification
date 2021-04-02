e2d3e7d9abe7a6069e1b4f39db456a9e680c0ef7
Jesper Brink <jesper@brinkandersen.dk>
2021-04-02 13:40:01 +0200

    aa

--- finetune_sbert/finetune_sbert.py
+++ finetune_sbert/finetune_sbert.py
@@ -2,7 +2,7 @@ from sentence_transformers import SentenceTransformer, losses
 from torch.utils.data import DataLoader
 from load_data import load_training_data, load_evaluator
 
-model_save_path = "stsb-distilbert-base-finetuned-feverdev3-scifact20"
+model_save_path = "stsb-distilbert-base-finetuned"
 scifact_claims_path = "../datasets/scifact/claims_sub_train.jsonl"
 scifact_val_claims_path = "../datasets/scifact/claims_validation.jsonl"
 scifact_corpus_path = "../datasets/scifact/corpus.jsonl"
@@ -10,29 +10,31 @@ scifact_corpus_path = "../datasets/scifact/corpus.jsonl"
 fever_claims_path = "../datasets/fever/fever_dev.jsonl"
 
 
-model = SentenceTransformer("stsb-distilbert-base")
-train_loss = losses.CosineSimilarityLoss(model)
+#model = SentenceTransformer("stsb-distilbert-base")
+#train_loss = losses.CosineSimilarityLoss(model)
 
 scifact_training_data, fever_training_data = load_training_data(scifact_claims_path, scifact_corpus_path, fever_claims_path)
 scifact_train_dataloader = DataLoader(scifact_training_data, shuffle=True, batch_size=32)
-fever_train_dataloader = DataLoader(fever_training_data, shuffle=True, batch_size=32)
+#fever_train_dataloader = DataLoader(fever_training_data, shuffle=True, batch_size=32)
 evaluator = load_evaluator(scifact_val_claims_path, scifact_corpus_path)
 
-model.fit(
-    train_objectives=[(fever_train_dataloader, train_loss)],
-    epochs=3,
-    evaluator=evaluator,
-    evaluation_steps=500,
-    output_path=model_save_path + "-fever",
-)
+#model.fit(
+#    train_objectives=[(fever_train_dataloader, train_loss)],
+#    epochs=3,
+#    evaluator=evaluator,
+#    evaluation_steps=500,
+#    output_path=model_save_path + "-fever",
+#)
 
-scifact_model = SentenceTransformer(model_save_path + "-fever")
+scifact_model = SentenceTransformer("stsb-distilbert-base")
+#scifact_model = SentenceTransformer(model_save_path + "-fever")
 scifact_train_loss = losses.CosineSimilarityLoss(scifact_model)
 
 scifact_model.fit(
     train_objectives=[(scifact_train_dataloader, scifact_train_loss)],
-    epochs=20,
+    epochs=100,
     evaluator=evaluator,
     evaluation_steps=500,
-    output_path=model_save_path + "-fever-scifact",
+    #output_path=model_save_path + "-fever-scifact",
+    output_path=model_save_path + "-scifact100",
 )
