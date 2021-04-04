25b441c54aab12513c4ac5f62e63c5c0665de847
Jesper Brink <jesper@brinkandersen.dk>
2021-04-04 17:12:21 +0200

    Removed bm25 from pipeline, as it is not working fully yet

--- competition/pipeline.py
+++ competition/pipeline.py
@@ -60,7 +60,6 @@ def pipeline(
 class FilterModel(enum.Enum):
     NONE = "none"
     SBERT_COSINE_SIMILARITY = "cosine"
-    BM25 = "bm25" # separate bm25, i.e. bm25-sent and bm25-abstract?
 
 class SentenceSelctionModel(enum.Enum):
     TWO_LAYER_DENSE = "twolayer"
