87a2b704244bab3b48663795f55853482cdb2c41
Mikkel S <mschou1306@gmail.com>
2021-04-06 20:39:00 +0200

    Add tokenization to stemming, do tokenization earlier in lemmatization

--- preprocessing/corpus/stemming.py
+++ preprocessing/corpus/stemming.py
@@ -1,10 +1,12 @@
 from nltk.stem import SnowballStemmer
+from nltk.tokenize import word_tokenize
 
 def stemming(doc):
+    # NOTE: Also tokenizes in order to split words and punctuation
     stemmer = SnowballStemmer("english")
     for sentence_idx, sentence in enumerate(doc["abstract"]):
         doc["abstract"][sentence_idx] = " ".join(
-                [stemmer.stem(w) for w in sentence.split()]
+                [stemmer.stem(w) for w in word_tokenize(sentence)]
             )
 
     return doc
