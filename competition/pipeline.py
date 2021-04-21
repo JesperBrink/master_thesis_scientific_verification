3bb24dfa14e6c4966192dbdd7c86c674db638fca
Mikkel Schou <33804514+mschou1306@users.noreply.github.com>
2021-04-21 11:02:09 +0200

    Feature/bm25 abstract retrieval (#28)
    * Add BM25 abstract retriever
    
    * Use searcher instead of indexer (~200 times faster on laptop)
    
    * Remove unncessary choice
    
    * run black
    
    * Fix comment

--- competition/pipeline.py
+++ competition/pipeline.py
@@ -8,6 +8,7 @@ from models.sentence_selection.cosineSimilarityModel import (
 )
 from models.stance_prediction.denseModel import TwoLayerDenseStancePredictor
 from models.abstract_retriever.tf_idf import TFIDFAbstractRetrieval
+from models.abstract_retriever.bm25 import BM25AbstractRetrieval
 import time
 import tensorflow as tf
 import enum
@@ -62,8 +63,8 @@ if __name__ == "__main__":
         "abstract_retriever",
         metavar="abstract_retriever",
         type=str,
-        choices=["dev", "noop", "tfidf"],
-        help="Which model to use for abstract retrieval. dev = quick testing, noop = No pruning, tfid = TF-IDF",
+        choices=["dev", "noop", "tfidf", "bm25"],
+        help="Which model to use for abstract retrieval. dev = quick testing, noop = No pruning, tfid = TF-IDF, bm25 = Pyserini BM25",
     )
     parser.add_argument(
         "sentence_selector",
@@ -106,6 +107,8 @@ if __name__ == "__main__":
         abstract_retriever = DevAbstractRetriever()
     elif args.abstract_retriever == "tfidf":
         abstract_retriever = TFIDFAbstractRetrieval(args.corpus_path)
+    elif args.abstract_retriever == "bm25":
+        abstract_retriever = BM25AbstractRetrieval(args.corpus_path)
 
     if args.sentence_selector == "lstm":
         sentence_selector = BertLSTMSentenceSelector(args.sentence_threshold)
---
+++ models/abstract_retriever/bm25.py
@@ -0,0 +1,66 @@
+from pyserini.search import SimpleSearcher
+from jnius import autoclass
+import jsonlines
+from tqdm import tqdm
+import os
+
+
+class BM25AbstractRetrieval:
+    """ Uses Pyserini BM25 to extract top k abstracts """
+
+    def __init__(self, corpus_path, k=75):
+        self.k = k
+        corpus_index_path = "bm25_abstract_index"  # Folder to write Pyserini index to
+        self.map_corpus_to_pyserini_index(corpus_path, corpus_index_path)
+        self.index_searcher = SimpleSearcher(corpus_index_path)
+        self.index_searcher.set_bm25(
+            0.9, 0.4
+        )  # Default params - does this even need to be set?
+        print("###  BM25 INIT DONE ###")
+
+    def __call__(self, claim_object, abstracts):
+        claim = claim_object["claim"]
+        hits = self.index_searcher.search(claim, k=self.k)
+
+        return {int(hit.docid): abstracts[int(hit.docid)] for hit in hits}
+
+    def map_corpus_to_pyserini_index(self, corpus_path, corpus_index_path):
+        mapped_corpus_path = "bm25_abstract_mapped_corpus"
+
+        if not os.path.exists(corpus_index_path):
+            os.makedirs(corpus_index_path)
+
+        if not os.path.exists(mapped_corpus_path):
+            os.makedirs(mapped_corpus_path)
+
+        mapped_corpus_file = open("{}/corpus.jsonl".format(mapped_corpus_path), "w")
+        writer = jsonlines.Writer(mapped_corpus_file)
+
+        # Map SciFact corpus to the format Pyserini wants
+        with jsonlines.open(corpus_path) as corpus:
+            for doc in tqdm(corpus):
+                doc_id = doc["doc_id"]
+                contents = " ".join(doc["abstract"])
+                writer.write({"id": str(doc_id), "contents": contents})
+
+        writer.close()
+        mapped_corpus_file.close()
+
+        # Create Pyserini index (for the searcher) from mapped corpus
+        args = [
+            "-collection",
+            "JsonCollection",
+            "-generator",
+            "DefaultLuceneDocumentGenerator",
+            "-threads",
+            "1",
+            "-input",
+            mapped_corpus_path,
+            "-index",
+            corpus_index_path,
+            "-storePositions",
+            "-storeDocvectors",
+            "-storeRaw",
+        ]
+        JIndexCollection = autoclass("io.anserini.index.IndexCollection")
+        JIndexCollection.main(args)
